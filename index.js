2c9da6e2e89957f1ae2b6d04644dc5619542e44a
Jonathan-Majors <izzymajors@gmail.com>
2021-03-19 09:28:57 -0500

    server up

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // start your server here
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+const PORT = 5000
+
+server.get('/', (req, res) => {
+    res.json({ message: 'response with on'})
+})
+
+server.listen(PORT, () => {
+   console.log(`\n*** server running on Port ${PORT}***`) 
+})
